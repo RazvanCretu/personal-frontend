cd1e52aa39f8084a579e42d54f283ee0053a40c6
Razvy Cretu <razvan.cretu97@gmail.com>
2021-08-16 05:08:08 +0300

    switch

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import Toggle from "../components/themeToggle";
 import Link from "next/link";
 import styled from "styled-components";
+import { lightTheme } from "../styles/theme";
 
 const Navigation = styled.nav`
   width: 100%;
@@ -25,7 +26,7 @@ const Navigation = styled.nav`
     text-decoration: underline;
   }
 
-  @media (max-width: 375px) {
+  @media (max-width: 450px) {
     bottom: 0;
     padding-bottom: 1rem;
   }
@@ -34,6 +35,45 @@ const Navigation = styled.nav`
 const BackButton = styled.a`
   left: 0;
   margin-left: 2rem;
+`;
+
+const StyledInput = styled.input`
+  &[type="checkbox"] {
+    position: relative;
+    width: 50px;
+    height: 30px;
+    appearance: none;
+    background: #c6c6c6;
+    outline: none;
+    border-radius: 20px;
+    box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+    transition: 0.5s;
+  }
+
+  &[type="checkbox"]:before {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    content: "";
+    position: absolute;
+    width: 30px;
+    height: 30px;
+    border-radius: 15px;
+    top: 0;
+    left: 0;
+    background: #fff;
+    transform: scale(1.1);
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+    transition: 0.5s;
+  }
+
+  &:checked[type="checkbox"] {
+    background: #03a9f4;
+  }
+
+  &:checked[type="checkbox"]:before {
+    left: 20px;
+  }
 `;
 
 const Layout = ({ children, theme, toggleTheme }) => {
@@ -45,7 +85,12 @@ const Layout = ({ children, theme, toggleTheme }) => {
             <BackButton>Back</BackButton>
           </Link>
         )}
-        <Toggle theme={theme} toggleTheme={toggleTheme} />
+        <StyledInput
+          type="checkbox"
+          onChange={toggleTheme}
+          checked={theme !== "light"}
+        />
+        {/* <Toggle theme={theme} toggleTheme={toggleTheme} /> */}
         <Link href="/projects">Projects</Link>
         <Link href="/about">About</Link>
         <Link href="/contact">Conact</Link>
