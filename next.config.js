8638a84bea731e909b02c41128376d8f5ada69fe
Razvy Cretu <razvan.cretu97@gmail.com>
2022-05-20 20:25:52 +0300

    removed .babelrc and add config in next.config

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ module.exports = {
   images: {
     domains: ["res.cloudinary.com", "image.flaticon.com", "wallsdesk.com"],
   },
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
 };
